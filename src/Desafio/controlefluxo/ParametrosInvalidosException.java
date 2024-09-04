4b1f492e5c425ccdb489af69f392aed3600f2395
WillianEPI <139443853+WillianEPI@users.noreply.github.com>
2024-09-04 13:18:52 -0300

    DesafioControleFluxo

---
+++ src/Desafio/controlefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Desafio.controlefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
